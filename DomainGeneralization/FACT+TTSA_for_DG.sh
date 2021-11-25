8b12c8e431b3c16f4e715098a7f9930ab56bf0c2
mx xie <39591668+Xiemixue@users.noreply.github.com>
2021-11-25 14:27:13 +0800

    Add files via upload

--- DomainGeneralization/FACT+TTSA_for_DG.sh
+++ DomainGeneralization/FACT+TTSA_for_DG.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # PACS
 python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/PACS/kfold/ --dset PACS --output_dir log/FACT+TTSA_for_DG/PACS/resnet18 --target_domain art_painting --epochs 50 --schdule_gamma 1  --lam_const 2 --lr 0.015 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
 python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/PACS/kfold/ --dset PACS --output_dir log/FACT+TTSA_for_DG/PACS/resnet18 --target_domain cartoon --epochs 50 --schdule_gamma 1  --lam_const 2 --lr 0.015 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
@@ -11,11 +12,12 @@ python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet50 --batch_size 16 --data
 python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet50 --batch_size 16 --data_dir /data1/TL/data/PACS/kfold/ --dset PACS --output_dir log/FACT+TTSA_for_DG/PACS/resnet50 --target_domain photo --epochs 50 --schdule_gamma 1  --lam_const 2 --lr 0.015 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
 python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet50 --batch_size 16 --data_dir /data1/TL/data/PACS/kfold/ --dset PACS --output_dir log/FACT+TTSA_for_DG/PACS/resnet50 --target_domain sketch --epochs 50 --schdule_gamma 1  --lam_const 2 --lr 0.015 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
 
+
 # Office-Home
-python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/office-home/ --dset office-home --output_dir log/FACT+TTSA_for_DG/home/resnet18 --target_domain Art --epochs 30 --gamma 10 --seed 2 --lam_const 100 --lr 0.005 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
-python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/office-home/ --dset office-home --output_dir log/FACT+TTSA_for_DG/home/resnet18 --target_domain Clipart --epochs 30 --gamma 10 --seed 2 --lam_const 100 --lr 0.005 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
-python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/office-home/ --dset office-home --output_dir log/FACT+TTSA_for_DG/home/resnet18 --target_domain Product --epochs 30 --gamma 10 --seed 2 --lam_const 100 --lr 0.005 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
-python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/office-home/ --dset office-home --output_dir log/FACT+TTSA_for_DG/home/resnet18 --target_domain RealWorld --epochs 30 --gamma 10 --seed 2 --lam_const 100 --lr 0.005 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
+python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/office-home/ --dset office-home --output_dir log/FACT+TTSA_for_DG/home/resnet18 --target_domain Art --epochs 30 --schdule_gamma 10 --seed 0 --lam_const 100 --lr 0.005 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
+python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/office-home/ --dset office-home --output_dir log/FACT+TTSA_for_DG/home/resnet18 --target_domain Clipart --epochs 30 --schdule_gamma 10 --seed 0 --lam_const 100 --lr 0.005 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
+python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/office-home/ --dset office-home --output_dir log/FACT+TTSA_for_DG/home/resnet18 --target_domain Product --epochs 30 --schdule_gamma 10 --seed 0 --lam_const 100 --lr 0.005 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
+python3 train_FACT+TTSA_for_DG.py --gpu 1 --arch resnet18 --batch_size 16 --data_dir /data1/TL/data/office-home/ --dset office-home --output_dir log/FACT+TTSA_for_DG/home/resnet18 --target_domain RealWorld --epochs 30 --schdule_gamma 10 --seed 0 --lam_const 100 --lr 0.005 --lambda0 0.25 --gamma 0.001 --eta 0.5 --alpha 0.1 --beta 0.1
 
 
 
